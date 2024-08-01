c6b9ed45cb4f2a62b559bbfb03a4fce7de48d4fc
Ivan Ershov <Navi133@yandex.ru>
2024-08-01 20:01:43 +0300

    Добавил JS файл

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let textArea = document.querySelector('.container__text-area')
 let paragraph = document.querySelector('.container__paragraph');
 
-console.log(textArea);
+const btn = document.querySelector('.btn');
+const inputAmnt = document.querySelector('.container__form-input_type_amnt');
+const inputPerc = document.querySelector('.container__form-input_type_perc');
 
+console.log(inputPerc);
+
+function showBadRes() {
+  paragraph.textContent = 'Oh shit, try again 🤨';
+  btn.style.backgroundColor = '#eb520b'; 
+}
+
+function showGoodRes() {
+  paragraph.textContent = `Oh shit, try again 🤨 ${calcPers(100000, 18)}`;
+  btn.style.backgroundColor = '#efa2ef'; 
+}
+
+function calcPers(Amnt, Perc) {
+  let value = Amnt * Perc / 365 / 100;
+  return value;
+}
+
+// showGoodRes();
+console.log(calcPers(100000, 18));
