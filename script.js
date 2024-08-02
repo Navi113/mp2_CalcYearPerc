de450a00ad906b73dfb01256a8dabfae97e9f1d7
Ivan Ershov <Navi133@yandex.ru>
2024-08-02 17:47:16 +0300

    Доработал скрипты

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 let textArea = document.querySelector('.container__text-area')
-let paragraph = document.querySelector('.container__paragraph');
-
-const btn = document.querySelector('.btn');
+const form = document.querySelector('.container__form');
 const inputAmnt = document.querySelector('.container__form-input_type_amnt');
 const inputPerc = document.querySelector('.container__form-input_type_perc');
+const btn = document.querySelector('.btn');
 
-console.log(inputPerc);
-
+// Функция показывает ошибку
 function showBadRes() {
-  paragraph.textContent = 'Oh shit, try again 🤨';
-  btn.style.backgroundColor = '#eb520b'; 
+  textArea.innerHTML = '<p class="container__paragraph">Ошибочка вышла! Что-то введено неправильно, попробуйте еще разик</p><span class="container__span">🫠</span>';
+  btn.style.backgroundColor = '#eb520b';
 }
 
-function showGoodRes() {
-  paragraph.textContent = `Oh shit, try again 🤨 ${calcPers(100000, 18)}`;
-  btn.style.backgroundColor = '#efa2ef'; 
+// Функция показывает верный результат
+function showGoodRes(day, month, year) {
+  textArea.innerHTML = `<p class="container__paragraph">С вкладом в ${inputAmnt.value} руб. и ${inputPerc.value}% вы получите:</p><p class="container__paragraph">в день: ${day} руб.</p><p>в месяц: ${month} руб.</p><p class="container__paragraph">в год: ${year} руб.</p><span class="container__span">😊</span>`;
+  btn.style.backgroundColor = '#efa2ef';
 }
 
+// Функция расчета процентов
 function calcPers(Amnt, Perc) {
-  let value = Amnt * Perc / 365 / 100;
-  return value;
+  let day = Amnt * Perc / 365 / 100;
+  let month = Amnt * Perc / 31 / 100;
+  let year = Amnt * Perc / 1 / 100;
+
+  return {
+    day: day.toFixed(2),
+    month: month.toFixed(2),
+    year: year.toFixed(2)
+  };
 }
 
-// showGoodRes();
-console.log(calcPers(100000, 18));
+// Функция очистки полей ввода
+function clearImputs() {
+  inputAmnt.value = '';
+  inputPerc.value = '';
+}
+
+// Функция проверки формы
+function getRes(evt) {
+  evt.preventDefault();
+
+  let valueAmnt = inputAmnt.value;
+  let valuePerc = inputPerc.value;
+
+  if (valueAmnt && valuePerc > 0) {
+    showGoodRes(
+      calcPers(valueAmnt, valuePerc).day,
+      calcPers(valueAmnt, valuePerc).month,
+      calcPers(valueAmnt, valuePerc).year
+    );
+    clearImputs()
+  } else {
+    showBadRes();
+    clearImputs()
+  }
+}
+
+// Слушатель на форму
+form.addEventListener('submit', getRes)
+
+console.log(inputAmnt.textContent);
